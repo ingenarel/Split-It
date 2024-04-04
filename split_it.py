2b80a7f58b13400adf93097589e78be728549f2a
ingenarel <145784829+ingenarel@users.noreply.github.com>
2024-04-04 08:04:39 +0600

    new version that uses functions that chaos made

--- split_it.py
+++ split_it.py
@@ -162,7 +162,7 @@ def error_msg():
 
 #The splitter is done
 def end():
-    print("""""Your splitting is finished. yayyyyyyy!!!!!
+    print("""Your splitting is finished. yayyyyyyy!!!!!
 
 ++++++++++++++++**++*%#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ++++++++++++++++@*#+###+%*#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -230,41 +230,45 @@ I hope you have a great life""")
 #Variables for runtime
 class variables:
     #The title Variable
-    title="Split It! version 0.8"
+    local_version="v0.8"
+    title=f"Split It! version {local_version}"
     
-    starting_choice=""
+    #URL Specific stuff
+    repo_owner = "ingenarel"
+    repo_name = "Split-It"
     
+    #folder specific variables for copying/other
     folder_name=""
-    folders=0
-    folder_lookup={"1": "config", "2": "data", "3": "mesh", "4": "guiding", "5": "noise", "6": "particles"}
     
+    folder_lookup={"1": "config", "2": "data", "3": "mesh", "4": "guiding", "5": "noise", "6": "particles"}
     current_folder=1
+    max_folders=6
+
     output_dir=""
     
     file_count=0
     source_folder=""
     folder_list=""
     source_files=""
+    source_file=""
     
     files_in_directory=""
-    destination_base_folder=""
+    destination_base_folder="Destination"
+    destination_folder="Destination"
     
     count=0
     folder_count=1
     
     exist_ok=True
-    
-    guiding_exists=False
-    noise_exists=False
-    particles_exist=False
 
-#Listing the folders
+#The function is for Listing the folders and setting the right folder (not yet checking if it is the right folder.)
 def list_folders():
     try:
         while True:
+            #the input for the folder to copy from.
             variables.folder_name=input("Please enter your foldername. (CASE SENSITIVE!)")#.lower() #Just in case you change your mind.
             variables.folder_list=[folder for folder in os.listdir('.') if os.path.isdir(folder)]
-            print(variables.folder_list)
+            #print(variables.folder_list) #For debugging if something goes to shit
             if variables.folder_name not in variables.folder_list:
                 error_msg()
                 
@@ -276,27 +280,276 @@ def list_folders():
 
 #Function for copying all the folders.
 def processing_folders():
-    if variables.folder_name in variables.folders:
+    while variables.current_folder<variables.max_folders+1:
         while True:
-            while True:
-                try:
-                    variables.file_count=int("Enter the number of files to copy in to each folder: ")
-                except TypeError:
-                    print(f"Nuh uh! Numbers only!\n")
-                
-                if variables.file_count > 1:
-                    print("Nuh uh! There has to be at least <ReplaceNumber> files. ")
-                
-                elif variables.file_count < 9:
-                    break
+            if variables.file_count==0:
+                while True:
+                    try:
+                        variables.file_count=int(input("Enter the number of files to copy in to each folder: "))
+                    except TypeError:
+                        print(f"Nuh uh! Numbers only!\n")
+                    
+                    if variables.file_count < 1:
+                        print("Nuh uh! There has to be at least 10 files. ")
+                    
+                    elif variables.file_count > 9:
+                        break
             
             variables.output_dir=str(variables.folder_lookup.get(str(variables.current_folder)))
             variables.source_folder=os.path.join(variables.folder_name, variables.output_dir)
-            variables.destination_base_folder="Destination"
+
+            print(f"Please wait while I split the {variables.folder_lookup.get(str(variables.current_folder))} files... >.<")
             
-            print(f"Please wait while I split the {variables.folder_lookup.get(str(variables.current_folder))} files...>.<")
+            try:
+                os.makedirs(os.path.join(variables.destination_base_folder, str(variables.folder_count), variables.folder_name, variables.output_dir), variables.exist_ok)
+            except FileExistsError:
+                pass
             
-            print
+            for filename in os.listdir(variables.source_folder):
+                variables.source_file = os.path.join(variables.source_folder, filename)
+                
+                #Copy files to the current destination folder
+                shutil.copy(variables.source_file, os.path.join(variables.destination_base_folder, str(variables.folder_count), variables.folder_name, variables.output_dir))
+                
+                variables.count+=1
+                
+                #If specified number of files are copied, reset the file counter and moce to the next destination folder
+                if variables.count == variables.file_count:
+                    variables.count=0
+                    variables.folder_count+=1
+                    try:
+                        os.makedirs(os.path.join(variables.destination_base_folder, str(variables.folder_count), variables.folder_name, variables.output_dir))
+                    except FileExistsError:
+                        pass
+            break
+        variables.count=0
+        variables.folder_count=1
+        variables.current_folder+=1
+        variables.output_dir=str(variables.folder_lookup.get(str(variables.current_folder)))
+        print(variables.current_folder)
+    variables.count=0
+    variables.folder_count=1
+    variables.current_folder=1
+    variables.output_dir=""
+    zip_files()
+
+
+
+#THIS IS THE ZIPPER AND I FUCKING HAD TO PUT IT HERE BECAUSE IT WASN'T FUCKING WORKING AND NOW I'M TESTING IT I JUST WANT TO FUCKING KILL MYSELF FUCK
+def zip_files():
+    print("Please wait while I zip the files... >.<")
+
+    # Change directory to the destination folder
+    os.chdir(variables.destination_folder)
+
+    # Loop through each folder in the destination folder
+    for variables.folder_name in os.listdir():
+        if os.path.isdir(variables.folder_name):
+            # Create a zip file with the same name as the folder
+            with zipfile.ZipFile(f"{variables.folder_name}.zip", "w") as zip_file:
+                # Add all files and empty folders in the folder to the zip file
+                for root, dirs, files in os.walk(variables.folder_name):
+                    # Add files
+                    for file in files:
+                        zip_file.write(os.path.join(root, file),
+                                    os.path.relpath(os.path.join(root, file),
+                                                    os.path.join(variables.folder_name)))
+                    # Add empty directories
+                    for dir in dirs:
+                        dir_path = os.path.relpath(os.path.join(root, dir),
+                                                os.path.join(variables.folder_name))
+                        zip_file.write(os.path.join(root, dir), dir_path)
+
+    print("All folders zipped successfully.")
+    ask_deletion()
+
+def ask_deletion():
+    while True:
+        print("""
+Do you want to delete the folders that were created when you split the project?
+The zip files won't be deleted, just the folders and files that were created in order to make those zips.""")
+        choice=input("y to delete and n to cancel: ").lower()[0:1]
+        
+        if choice=="y":
+            # Loop through all items in the folder and delete them
+            for item in os.listdir():
+                if os.path.isdir(item):
+                    os.system(f"rd /s /q \"{item}\"")
+            print("\nAll split cache folders deleted successfully.\nDon't worry, the zips aren't deleted.")
+            break
+        
+        elif choice=="cls" or choice=="clear":
+            os.system('cls' if os.name == 'nt' else 'clear')
+        
+        else:
+            print('Error: Please use euther "y" or "n"')
+    end()
+
+
+def helpsite():
+    print("""
+The main commands:
+    s or start: Starts the program main feature.
+    cls or clear: Clears the screen.
+    ver or version: Checks for updates
+Other commands:
+    exit() or exit or close: Closes the program.
+    """)
+
+def update_check(**kwargs):
+    try:
+        response=requests.get(kwargs["url"])
+        if response.status_code == 200:
+            latest_release = response.json()
+            latest_version = latest_release.get('tag_name')
+
+        else:
+            if response.status_code==400:
+                print("""ERROR 400: bad request.
+
+This status code indicates that there's something off with the request you sent to the server.
+It's like filling out a form incorrectly or forgetting to provide essential details.
+The server couldn't understand or process your request due to missing or malformed data.
+""")
+        
+            elif response.status_code == 401:
+                print("""ERROR 401: Unauthorized
+This status code means you're not allowed to access the requested resource without proper authentication.
+It's like trying to enter a restricted area without showing your ID.
+You need to provide valid credentials, such as a username and password or an authentication token, to gain access.
+""")
+        
+            elif response.status_code == 403:
+                print("""ERROR 403: Unauthorized
+
+You're being denied access, plain and simple.")
+Even with valid credentials, you're not allowed to access the resource because you lack the necessary permissions.
+It's like trying to enter a building without the proper authorization or access card.
+You need to request permission from the appropriate authorities.
+""")
+        
+            elif response.status_code == 404:
+                print("""ERROR 404: Unauthorized.
+
+This status code indicates that the server couldn't find the resource you requested.
+It's like looking for a book on a library shelf only to discover it's not there. 
+The resource may have been moved, deleted, or simply never existed. 
+Double-check the URL or try searching for the resource in a different location.
+""")
+        
+            elif response.status_code == 422:
+                print("""ERROR 422: Unauthorized.
+
+Ah, it seems there's a problem with the data you provided. ")
+This status code typically occurs when the server understands your request but can't process it due to invalid data.
+It's like trying to fill out a form with incorrect or incomplete information. 
+Review the data you submitted and ensure it meets the server's requirements.
+""")
+            
+            elif response.status_code == 429:
+                print("""ERROR 429: Unauthorized.
+
+Slow down there! You've been making too many requests to the server within a short period. ")
+This status code indicates that you've hit a rate limit, and the server is asking you to ease up for a bit. ")
+It's like trying to take too many slices of cake at once, and someone politely suggests you wait your turn.")
+""")
+        
+            elif response.status_code == 500:
+                print("""ERROR 500: Server Side Error.
+
+Uh-oh, something went wrong on the server's end, and it's not your fault. 
+This status code indicates an unexpected problem occurred while the server was trying to process your request.
+It's like ordering food at a restaurant and having the kitchen catch fire. 
+The server is apologizing for the inconvenience and asking for your patience while they sort things out.
+""")
+        
+            elif response.status_code == 503:
+                print("""ERROR 503 Unauthorized:
+
+Hold your horses! The server is currently unavailable to handle your request.
+This status code typically occurs due to maintenance or overload.
+It's like calling a store outside of business hours and getting a message saying they're closed for the day.
+You'll need to try again later when the server is back up and running.
+""")
+            
+            else:
+                print(f"I Don't know what the fuck are you doing but I can't find an error code for you.\n")
+    except requests.exceptions.RequestException:
+        print("""
+Failed to retrieve the latest version from GitHub.
+Please check your internet connection.""")
+        input("Press enter to return to the main menu.")
+        return
+    
+    if latest_version:
+        print(f"Current version: {variables.local_version}.")
+        print(f"Latest version: {latest_version}.")
+        
+        if variables.local_version>latest_version:
+            print("A newer version than expected is there.")
+    
+        elif variables.local_version==latest_version:
+            print("You are already using the latest version.")
+        
+        else:
+            while True:
+                print("A new version is available. Do you want to download it?")
+                print("You if you want to use the latest version, you need to start it from the downloaded version though.")
+                print("It creates a folder called latest_build and stores the zip there.")
+                choice = input("press y to download and n to cancel: ")
+                
+                if choice.lower() == "y":
+                    # Download the latest build
+                    assets = latest_release.get('assets', [])
+                    
+                    if assets:
+                        # Create a folder to store the downloaded files
+                        download_folder = "latest_build"
+                        os.makedirs(download_folder, exist_ok=True)
+
+                        for asset in assets:
+                            download_url = asset.get('browser_download_url')
+                            download_response = requests.get(download_url)
+                                
+                            if download_response.status_code == 200:
+                                # Save the file
+                                file_path = os.path.join(download_folder, asset.get('name'))
+                                with open(file_path, 'wb') as file:
+                                    file.write(download_response.content)
+                                print(f"Downloaded: {asset.get('name')}")
+                                break
+
+                            else:
+                                print(f"Failed to download: {asset.get('name')}")
+                                input("press enter to go back to simulation folder select")
+                                break
+
+                        print(f"All assets from the latest release have been downloaded to '{download_folder}'.")
+                        input("press enter to go back to simulation folder select")
+                        break
+
+                    else:
+                        print("No assets found in the latest release.")
+                        input("press enter to go back to simulation folder select")
+                        break
+                elif choice.lower() == "n":
+                    print("Skipping download.")
+                    input("press enter to go back to simulation folder select")
+                    break
+
+                elif choice.lower() in ["cls", "clear"]:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    continue
+
+                else:
+                    print("Invalid choice. Please enter 'yes' or 'no'.")
+                    continue
+    else:
+        print("Failed to retrieve the latest version from GitHub. Please check your internet connection and try again.")
+        input("press enter to go back to the main menu.")
+
+        return
+    
 
 def main_func():
     while True:
@@ -306,11 +559,17 @@ You can type "cls" or "clear screen" to clean the Terminal/Command Prompt.
 Type "s" or "start" to use the splitter.
 type "help" for a more thorough help site.""")
         
-        variables.starting_choice=input("What do you want to do? ")
+        starting_choice=input("What do you want to do? ").lower()
         
-        if variables.starting_choice=="s" or variables.starting_choice=="start":
+        if starting_choice=="s" or starting_choice=="start":
             list_folders()
             processing_folders()
+        
+        elif starting_choice=="ver" or starting_choice=="version":
+            update_check(url=f"https://api.github.com/repos/{variables.repo_owner}/{variables.repo_name}/releases/latest")
+        
+        elif starting_choice=="exit()" or starting_choice=="exit" or starting_choice=="close":
+            exit()
 
 if __name__=="__main__":
     start(title=variables.title)

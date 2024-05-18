79c89a8dba92fee7a2a641d64847c679049c076c
ingenarel <ingenarelitems@gmail.com>
2024-05-18 03:09:43 +0000

    May 18, 2024, 9:09 AM

--- grettings.py
+++ grettings.py
@@ -50,7 +50,7 @@ ________________________________________________________________________________
 |:____________________________________________________________________________________________________________________________________________________________________________:|
 ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
-For the best visual experience, please Maximise the Terminal/Command Prompt.
+For the best visual experience, please Maximise the Terminal/Command Prompt. And if the fonts look messy, install nerdfonts.
 
 BEFORE YOU START THIS MAKE SURE THAT YOU HAVE DOUBLE THE EMPTY SPACE IN YOUR DRIVE AS YOUR BLEND AND CACHE FILE COMBINED!
 THAT MEANS IF YOUR BLEND FILE IS 100 mb AND YOUR SIMULATION CACHE FOLDER IS 900 mb, MAKE SURE THAT YOU HAVE ATLEAST 2 gb OF EMPTY SPACE IN YOUR DRIVE!
---
+++ split_it.py
@@ -0,0 +1,7 @@
+from grettings import grettings
+
+def main():
+    print()
+
+if __name__ == "__main__":
+    main()

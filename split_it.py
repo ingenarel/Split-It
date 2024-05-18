2262f722a158e6318f384cbc5b1ef0d880ea8aed
ingenarel <ingenarelitems@gmail.com>
2024-05-18 22:42:09 +0000

    May 19, 2024, 4:42 AM

--- split_it.py
+++ split_it.py
@@ -282,7 +282,7 @@ def main():
                 commands.add("help")
             elif argument in ["credits", "c"]:
                 commands.add("credits")
-            elif re.search(argument, r"^(?:s|fff)$"):
+            elif re.match(argument, r"(s)"):
                 commands.add(argument)
             else:
                 print(f"\"{argument}\" isn't a valid command. skipping it.")

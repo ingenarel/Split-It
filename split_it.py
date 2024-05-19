c7b8021b9bb5e520723d4029fa4f86a969f06e47
ingenarel <ingenarelitems@gmail.com>
2024-05-19 22:41:08 +0000

    May 20, 2024, 4:41 AM

--- split_it.py
+++ split_it.py
@@ -271,8 +271,8 @@ def __spliter__(files_in_dict, blend_file):
     # print(files_in_dict)
     # print(blend_file_size)
     for folders in files_in_dict:
-        x = 0
         for files in files_in_dict[folders]:
+            x = 0
             while x < 3:
                 print(files_in_dict[folders][x])
                 x += 1

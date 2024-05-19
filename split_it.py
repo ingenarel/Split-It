8bbd95a7f7608d18166c1a06019c2b2cf2528985
ingenarel <ingenarelitems@gmail.com>
2024-05-19 14:28:57 +0000

    May 19, 2024, 8:28 PM

--- split_it.py
+++ split_it.py
@@ -268,10 +268,10 @@ Do you want to continue? press y to to continue and n to cancel
 
 def __spliter__(files_in_dict, blend_file):
     blend_file_size = genericpath.getsize(blend_file)
-    print(files_in_dict)
+    # print(files_in_dict)
     # print(blend_file_size)
-    # for folders in files_in_dict:
-    #     # print(files_in_dict[folders])
+    for folders in files_in_dict:
+        print(files_in_dict[folders][0])
     #     x = 0
     #     for files in files_in_dict[folders]:
     #         print(files)

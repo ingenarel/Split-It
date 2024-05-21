dc78df635c3d20c2ef082753151ac064bcb1043e
ingenarel <ingenarelitems@gmail.com>
2024-05-21 00:31:40 +0000

    May 21, 2024, 6:31 AM

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ cachefolder_path = "L:\\cachesplittertest\\LiquidSim"
 blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
 for folders in os.listdir(cachefolder_path):
-    for files in os.listdir(f"{cachefolder_path}\\{folders}"):
-        print(f"{folders}=>{files}")
+    try:
+        print(os.listdir(f"{cachefolder_path}\\{folders}")[0])
+    except IndexError:
+        pass
+    # for files in os.listdir(f"{cachefolder_path}\\{folders}"):
+    #     print(f"{folders}=>{files}")

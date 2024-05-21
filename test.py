5cce2b6eb63e17c9dfe56b1254a7f6bdabef5415
ingenarel <ingenarelitems@gmail.com>
2024-05-21 00:29:16 +0000

    May 21, 2024, 6:29 AM

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
 for folders in os.listdir(cachefolder_path):
     for files in os.listdir(f"{cachefolder_path}\\{folders}"):
-        print(files)
+        print(f"{folders}=>{files}")

3b3351c5c0e5108b105cdec682094ac886b25e49
ingenarel <ingenarelitems@gmail.com>
2024-05-21 00:38:16 +0000

    May 21, 2024, 6:38 AM

--- test.py
+++ test.py
@@ -3,12 +3,11 @@ import os
 cachefolder_path = "L:\\cachesplittertest\\LiquidSim"
 blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
+last_shit = 0
 for folders in os.listdir(cachefolder_path):
     try:
         counter = 0
         while counter < 3:
-            if counter == 2:
-                counter = 0
             print(os.listdir(f"{cachefolder_path}\\{folders}")[counter])
             counter += 1
     except IndexError:

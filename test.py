2bf2ebb939a450a15647c516624bb8aeaf16b969
ingenarel <ingenarelitems@gmail.com>
2024-05-21 00:35:06 +0000

    May 21, 2024, 6:35 AM

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
 for folders in os.listdir(cachefolder_path):
     try:
-        print(os.listdir(f"{cachefolder_path}\\{folders}")[0])
+        counter = 0
+        while counter < 3:
+            if counter == 2:
+                counter == 0
+            print(os.listdir(f"{cachefolder_path}\\{folders}")[counter])
+            counter += 1
     except IndexError:
         pass
     # for files in os.listdir(f"{cachefolder_path}\\{folders}"):

9dd20d81c190cdb2c0a78c4f6db4bb4b130227e7
ingenarel <ingenarelitems@gmail.com>
2024-05-21 00:28:12 +0000

    May 21, 2024, 6:28 AM

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+import os
+
 cachefolder_path = "L:\\cachesplittertest\\LiquidSim"
 blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
+for folders in os.listdir(cachefolder_path):
+    for files in os.listdir(f"{cachefolder_path}\\{folders}"):
+        print(files)

35908df85a80827fa1aa4b090c384e0e19793965
ingenarel <ingenarelitems@gmail.com>
2024-06-09 19:08:13 +0600

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,3 @@ cachefolder_path = "L:\\cachesplittertest\\LiquidSim"
 blendfile_path = "l:\\cachesplittertest\\kaboom.blend"
 
 for folders in os.listdir(cachefolder_path):
-    try:
-        last_shit = 0
-        counter = 0
-        while counter < 3:
-            if counter == 3:
-                counter = 0
-                break
-            print(os.listdir(f"{cachefolder_path}\\{folders}")[last_shit])
-            counter += 1
-            last_shit += 1
-    except IndexError:
-        pass
-    # for files in os.listdir(f"{cachefolder_path}\\{folders}"):
-    #     print(f"{folders}=>{files}")
